4c207866642b1a0e47a430ac1a3c7b99dde69bef
BolfPatrik <bolfpatrik0@gmail.com>
2021-11-27 20:57:48 +0100

    Kartica,broj kartice
    Broj kartice stavljen u trenera i polaznika, kartica kao entitet izbačen

--- teretana.sql
+++ teretana.sql
@@ -6,16 +6,9 @@ create table polaznik(
     sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
-    oib varchar(11)
-);
-
-create table kartica(
-    sifra int not null primary key auto_increment,
-    brojkartice varchar(50),
-    adresa varchar(50),
-    telefonteretane varchar(50),
-    polaznik int,
-    trener int
+    oib varchar(11),
+    brojkartice varchar(50)
+    
 );
 
 create table trening(
@@ -35,11 +28,10 @@ create table trener(
     prezime varchar(50),
     oib varchar(11),
     iban varchar(50),
-    placa varchar(50)
+    placa varchar(50),
+    brojkartice varchar(50)
 );
 
-alter table kartica add foreign key (polaznik) references polaznik(sifra);
-alter table kartica add foreign key (trener) references trener(sifra);
 
 alter table trening add foreign key (trener) references trener(sifra);
 alter table trening add foreign key (polaznik) references polaznik(sifra);
