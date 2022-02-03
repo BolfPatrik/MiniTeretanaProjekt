7d3efa85698e219d58abfc302d3f3e3533edd24f
Patrik Bolf <93612143+BolfPatrik@users.noreply.github.com>
2022-02-03 22:10:07 +0100

    Create Polaznik.java

---
+++ JAVA/Polaznik.java
@@ -0,0 +1,36 @@
+package moj.modul;
+
+public class Polaznik extends Entitet{
+	
+	private String ime;
+	private String prezime;
+	private String oib;
+	private String brojkartice;
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getOib() {
+		return oib;
+	}
+	public void setOib(String oib) {
+		this.oib = oib;
+	}
+	public String getBrojkartice() {
+		return brojkartice;
+	}
+	public void setBrojkartice(String brojkartice) {
+		this.brojkartice = brojkartice;
+	}
+	
+	
+
+}
