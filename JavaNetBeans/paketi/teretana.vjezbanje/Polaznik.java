cbb2d7ab87e43b224db74e0175517e4cca40037c
Patrik Bolf <93612143+BolfPatrik@users.noreply.github.com>
2022-02-10 19:30:34 +0100

    Za sada samo test (treba poraditi na tome da radi)

---
+++ JavaNetBeans/paketi/teretana.vjezbanje/Polaznik.java
@@ -0,0 +1,45 @@
+package teretana.vjezbanje;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Polaznik extends Entitet{
+	
+        @Id
+	private String ime;
+	private String prezime;
+	private String oib;
+	private String brojkartice;
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getOib() {
+		return oib;
+	}
+	public void setOib(String oib) {
+		this.oib = oib;
+	}
+	public String getBrojkartice() {
+		return brojkartice;
+	}
+	public void setBrojkartice(String brojkartice) {
+		this.brojkartice = brojkartice;
+	}
+	
+@Override
+public String toString() {
+	return ime + " " + prezime;
+}	
+
+}
