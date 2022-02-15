f9a58430992d49e2defc8dd45791af86f26ebdc0
Patrik Bolf <93612143+BolfPatrik@users.noreply.github.com>
2022-02-15 20:06:14 +0100

    Update Start.java

--- JavaNetBeans/paketi/Start.java
+++ JavaNetBeans/paketi/Start.java
@@ -2,11 +2,21 @@ package edunova;
 
 
 
+import org.hibernate.Session;
 import util.HibernateUtil;
 
+
 public class Start {
+    private Session session;
+    
+    public Start(){
+        this.session = HibernateUtil.getSession();
+    }
+    
+    
     public static void main(String[] args) {
-        HibernateUtil.getSession();
+        new Start();
     }
     
 }
+
